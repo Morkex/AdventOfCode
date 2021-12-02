17c4b5fc43ed0017a6eb9f8882cff24289ea6096
Pablo Ruiz Jurado <Pablo.ruizjurado@gmail.com>
2021-12-02 01:49:03 +0100

    Inicio

---
+++ 01/aoc01.py
@@ -0,0 +1,36 @@
+file = "C:/Users/morke/Desktop/AdventOfCode/01/input.txt"
+
+
+def readFile(file):
+    list = []
+    f = open(file)
+    for x in f:
+        list.append(int(x))
+    f.close()
+    return list
+
+
+def countIncrementsPart1(list):
+    count = 0
+    for i, actualDepth in enumerate(list):
+        if i == 0:
+            continue
+        if actualDepth > list[i-1]:
+            count += 1
+    return count
+
+
+def countIncrementsPart2(list):
+    list3 = []
+    for i, actualDepth in enumerate(list):
+        actualDepth = list[i] + list[i+1] + list[i+2]
+        list3.append(actualDepth)
+        if i == len(list)-3:
+            break
+    return list3
+
+
+print("Part1:")
+print(countIncrementsPart1(readFile(file)))
+print("Part2:")
+print(countIncrementsPart1(countIncrementsPart2(readFile(file))))
